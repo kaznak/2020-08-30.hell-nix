0b8652d58e50c692c53c7f4d3efbe0efa279f98c
kaznak <kaznak.at.work@gmail.com>
2020-08-30 06:31:54 +0900

    hello.nix: more generic

--- hello.nix
+++ hello.nix
@@ -1,11 +1,12 @@
 with (import <nixpkgs> {});
-derivation {
+let drv = rec {
   name = "hello";
   builder = "${bash}/bin/bash";
-  args = [ ./hello_builder.sh ];
-  inherit gnutar gzip gnumake coreutils gawk gnused gnugrep;
+  args = [ ./builder.sh ];
+  buildInputs = [ gnutar gzip gnumake gcc binutils coreutils gawk gnused gnugrep ];
   gcc = clang;
   binutils = clang.bintools.bintools_bin;
   src = ./hello-2.10.tar.gz;
   system = builtins.currentSystem;
-}
+};
+in derivation drv
